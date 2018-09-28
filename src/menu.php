6c5784e4725553dda059cf2ca5f6c65f3393f956
Koen Sengers <k.sengers@gynzy.com>
2018-09-28 16:23:10 +0200

    Add css and extra index

---
+++ src/menu.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+  <html lang="nl">
+    <head>
+      <title>De Vooruitgang</title>
+      <meta charset="UTF-8">
+      <meta name="keywords" content="restaurant, bar, eetcafé, markt, eindhoven, eten, drinken, stappen">
+      <meta name="description" content="Restaurant en bar op de markt in  Eindhoven">
+      <link href="style_sheet.css" rel="stylesheet">
+    </head>
+
+        <body class="menu_kaart">
+          <div class="header">
+            <div class="menu">
+              <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a class="active" href="menu.php">Menu</a></li>
+            </ul>
+            </div>
+          </div>
+
+          <div class="background">
+          </div>
+        <div class="content_container_menu">
+          <img class="logo" src="images/logo.png" alt="img"/>
+          <div class="rbda"><a href="https://restaurantandbardesignawards.com/2016/entries/de-vooruitgang" target="_blank">
+            <img src="images/RBDA_logo_shortlist_.jpg" alt="img"></a></div>
+        </div>
+
+        <div id="menu_kaart">
+          <div class="hr"><hr></div>
+          <div class="hr1"><hr></div>
+          <div class="hr2"><hr></div>
+          <div class="hr3"><hr></div>
+          <div class="hr4"><hr></div>
+          <table class="menu_kaart_1">
+            <tr>
+              <td colspan="2" class="row_title"><h1 class="row_title">Dagkaart</h1></td>
+            </tr>
+            <tr>
+              <td colspan="2" ><h3 class="row_title_1">Te bestellen tot 17 uur</h3></td>
+            </tr>
+            <tr>
+              <td colspan="2" class="subtitle"><strong>Ontbijtkistje</strong></td>
+            </tr>
+            <tr>
+              <td class="subtitle_1"><strong>ONTBIJT</strong></td>
+              <td class="price"><strong>-8,50-</strong></td>
+            </tr>
+            <tr>
+              <td colspan="2" class="content">Keuze uit Nederlands of Engels ontbijt.</td>
+            </tr>
+          </table>
+          <br>
+          <br>
+          <table class="menu_kaart_1">
+            <tr>
+              <td class="subtitle"><strong>Boterhammen</strong></td>
+            </tr>
+            <tr>
+              <td class="subtitle"><strong>Keuze uit desem wit, donkervolkoren of spelt</strong></td>
+            </tr>
+          </table>
+          <table class="menu_kaart_1">
+            <tr>
+              <td class="subtitle_1"><strong>OUDE KAAS(VEG)</strong></td>
+              <td class="price_1"><strong>-6,00-</strong></td>
+            </tr>
+            <tr>
+              <td colspan="2" class="content">sla | tomaat | komkommer | rode ui</td>
+            </tr>
+            <tr>
+              <td class="subtitle_1"><strong>KIP</strong></td>
+              <td class="price_1"><strong>-7,50-</strong></td>
+            </tr>
+            <tr>
+              <td colspan="2" class="content">rucola | mangosalsa | chipotlemayo</td>
+            </tr>
+            <tr>
+              <td class="subtitle_1"><strong>CARPACCIO</strong></td>
+              <td class="price_1"><strong>-9,50-</strong></td>
+            </tr>
+            <tr>
+              <td colspan="2" class="content">parmezaan | pitten | truffelmayo</td>
+            </tr>
+            <tr>
+              <td class="subtitle_1"><strong>ZALM</strong></td>
+              <td class="price_1"><strong>-9,50-</strong></td>
+            </tr>
+            <tr>
+              <td colspan="2" class="content">gerookt | kappertjes | romeinse sla | komkommer zoetzuur | mierikswortelcr&egrave;me</td>
+            </tr>
+            <tr>
+              <td class="subtitle_1"><strong>TONIJNSALADE</strong></td>
+              <td class="price_1"><strong>-8,50-</strong></td>
+            </tr>
+            <tr>
+              <td colspan="2" class="content">ei | romeinse sla | rode ui</td>
+            </tr>
+            <tr>
+              <td class="subtitle_1"><strong>RUNDERKROKET</strong></td>
+              <td class="price_1"><strong>-7,50-</strong></td>
+            </tr>
+            <tr>
+              <td colspan="2" class="content">2 stuks | mosterd</td>
+            </tr>
+            </table>
+            <br>
+            <br>
+            <table class="menu_kaart_1">
+            <tr>
+              <td class="subtitle"><strong>Croques</strong></td>
+            </tr>
+            <tr>
+              <td class="subtitle"><strong>Dikke boterhammen gegratineerd uit de oven</strong></td>
+            </tr>
+            </table>
+            <br>
+            <table class="menu_kaart_1">
+            <tr>
+              <td class="subtitle_1"><strong>MONSIEUR</strong></td>
+              <td class="price_1"><strong>-5,50-</strong></td>
+            </tr>
+            <tr>
+              <td colspan="2" class="content">ham | kaas</td>
+            </tr>
+            <tr>
+              <td class="subtitle_1"><strong>MADAME</strong></td>
+              <td class="price_1"><strong>-7,00-</strong></td>
+            </tr>
+            <tr>
+              <td colspan="2" class="content">ham | kaas | spiegelei</td>
+            </tr>
+            <tr>
+              <td class="subtitle_1"><strong>DE MARKT</strong></td>
+              <td class="price_1"><strong>-8,50-</strong></td>
+            </tr>
+            <tr>
+              <td colspan="2" class="content">pittig gehakt | ham | kaas | chipotlemayo</td>
+            </tr>
+            </table>
+            <br>
+            <br>
+            <br>
+            <table class="menu_kaart_1">
+            <tr>
+              <td class="subtitle"><strong>Sandwiches</strong></td>
+            </tr>
+            <tr>
+              <td class="subtitle"><strong>Maisbrood</strong></td>
+            </tr>
+            </table>
+            <table class="menu_kaart_1">
+            <tr>
+              <td class="subtitle_1"><strong>UITSMIJTER</strong></td>
+              <td class="price_1"><strong>-6,50-</strong></td>
+            </tr>
+            <tr>
+              <td class="subtitle_1"><strong>UITSMIJTER</strong></td>
+              <td class="price_1"><strong>-7,00-</strong></td>
+            </tr>
+            <tr>
+              <td colspan="2" class="content">ham | of kaas | of spek | extra beleg + 0,50</td>
+            </tr>
+            <tr>
+              <td class="subtitle_1"><strong>UITSMIJTER 'DE VOORUITGANG'</strong></td>
+              <td class="price_1"><strong>-9,00-</strong></td>
+            </tr>
+            <tr>
+              <td colspan="2" class="content">ham | kaas | spek | champignons | tomaat</td>
+            </tr>
+            </table>
+            <br>
+            <br>
+            <br>
+            <table class="menu_kaart_1">
+            <tr>
+              <td class="subtitle"><strong>Lunchspecials</strong></td>
+            </tr>
+            </table>
+            <table class="menu_kaart_1">
+            <tr>
+              <td class="subtitle_1"><strong>PULLED PORK</strong></td>
+              <td class="price_1"><strong>-8,50-</strong></td>
+            </tr>
+            <tr>
+              <td colspan="2" class="content">pita | sla | koolsla | bbq saus</td>
+            </tr>
+            <tr>
+              <td class="subtitle_1"><strong>HAUTE DOG</strong></td>
+              <td class="price_1"><strong>-8,50-</strong></td>
+            </tr>
+            <tr>
+              <td colspan="2" class="content">bun | augurk | rode ui | jalape&ntilde;o | chipotlemayo</td>
+            </tr>
+            <tr>
+              <td class="subtitle_1"><strong>TUNA MELT</strong></td>
+              <td class="price_1"><strong>-7,50-</strong></td>
+            </tr>
+            <tr>
+              <td colspan="2" class="content">rode ui | tomaat | gegratineerde kaas</td>
+            </tr>
+            <tr>
+              <td class="subtitle_1"><strong>PEKING EEND</strong></td>
+              <td class="price_1"><strong>-8,00-</strong></td>
+            </tr>
+            <tr>
+              <td colspan="2" class="content">flensje | bosui | komkommer | sla | hoisin</td>
+            </tr>
+            <tr>
+              <td class="subtitle_1"><strong>FALAFEL(VEG)</strong></td>
+              <td class="price_1"><strong>-8,50-</strong></td>
+            </tr>
+            <tr>
+              <td colspan="2" class="content">pita | rode ui | sla | jalape&ntilde;o | tzatziki</td>
+            </tr>
+            <tr>
+              <td class="subtitle_1"><strong>BIG BURGER</strong></td>
+              <td class="price_2"><strong>-14,50-</strong></td>
+            </tr>
+            <tr>
+              <td colspan="2" class="content">sla | spek | tomaat | friet | bbq saus</td>
+            </tr>
+            <tr>
+              <td class="subtitle_1"><strong>MEXICAN BURGER</strong></td>
+              <td class="price_2"><strong>-15,00-</strong></td>
+            </tr>
+            <tr>
+              <td colspan="2" class="content">sla | cheddar | salsa | friet | guacamole</td>
+            </tr>
+            <tr>
+              <td class="subtitle_1"><strong>NACHOS(VEG)</strong></td>
+              <td class="price_1"><strong>-6,50-</strong></td>
+            </tr>
+            <tr>
+              <td colspan="2" class="content">kaas | tomaat | jalape&ntilde;o | zure room | guacamole</td>
+            </tr>
+            <tr>
+              <td colspan="2" class="content">met kip + 2,00</td>
+            </tr>
+          </table>
+          <br>
+          <br>
+        </div>
+
+        <div class="social_media_menu">
+          <a href="https://www.facebook.com/DeVooruitgang" target="_blank"><img src="images/facebook_icon.png" alt="" class="img-responsive"></a>
+            <a href="https://twitter.com/DeVooruitgang" target="_blank"><img src="images/twitter_icon.png" alt="" class="img-responsive"></a>
+            <a href="https://instagram.com/de_vooruitgang" target="_blank"><img src="images/instagram_icon.png" alt="" class="img-responsive"></a>
+          </div>
+          <div class="openings_tijden_menu">
+            <div class="openings_tijden_r1">Openingstijden:<br></div><div class="openings_tijden_r2"> Zondag &amp; Maandag<br></div><div class="openings_tijden_r3"> 10.00 &dash; 01.00 uur<br></div>
+            <div class="openings_tijden_r4">Dinsdag t/m Donderdag<br></div><div class="openings_tijden_r5"> 10.00 &dash; 02.00 uur<br></div>
+            <div class="openings_tijden_r6"> Vrijdag &amp; Zaterdag<br></div><div class="openings_tijden_r7"> 10.00 &dash; 03.00 uur</div>
+          </div>
+          <div class="adres_menu">
+            <div class="adres_r1"><p>Markt 11<br></div><div class="adres_r2">5611 EB Eindhoven<br></div><div class="adres_r3">T 040 243 3995<br></div><div class="adres_r4">info&commat;de&dash;vooruitgang.nl</div>
+      </div>
+
+      <div class="footer">
+      <div class="footer_text">&copy; 2016 DE VOORUITGANG
+        | EN IN BEHEER VAN KOEN SENGERS.</div>
+      </div>
+
+      </body>
+    </html>
