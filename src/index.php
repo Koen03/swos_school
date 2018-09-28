f0eee63351f567108ec9ea3d91c26ef8785814e7
Koen Sengers <k.sengers@gynzy.com>
2018-09-28 17:20:23 +0200

    codacy test

--- src/index.php
+++ src/index.php
@@ -2,8 +2,51 @@
   <html lang="nl">
     <head>
       <title>De Vooruitgang</title>
+      <meta charset="UTF-8">
+      <meta name="keywords" content="restaurant, bar, eetcafé, markt, eindhoven, eten, drinken, stappen">
+      <meta name="description" content="Restaurant en bar op de markt in  Eindhoven">
+      <link href="style_sheet.css" rel="stylesheet">
     </head>
-        Hallo
+
+    <body class="home">
+      <div class="header">
+        <div class="menu">
+          <ul>
+            <li><a class="active" href="index.php">Home</a></li>
+            <li><a href="menu.php">Menu</a></li>
+            <li><a href="over_ons.php">Over ons</a></li>
+        </ul>
+        </div>
+      </div>
+
+      <div class="background">
+      </div>
+
+    <div class="content_container">
+      <img class="logo" src="images/logo.png" alt="img"/>
+      <img class="voorpagina" src="images/voorpagina.jpg" alt="img"/>
+      <div class="rbda"><a href="https://restaurantandbardesignawards.com/2016/entries/de-vooruitgang" target="_blank">
+        <img src="images/RBDA_logo_shortlist_.jpg" alt="img"></a></div>
+      </div>
+      <div class="social_media">
+        <a href="https://www.facebook.com/DeVooruitgang" target="_blank"><img src="images/facebook_icon.png" alt="" class="img-responsive"></a>
+          <a href="https://twitter.com/DeVooruitgang" target="_blank"><img src="images/twitter_icon.png" alt="" class="img-responsive"></a>
+          <a href="https://instagram.com/de_vooruitgang" target="_blank"><img src="images/instagram_icon.png" alt="" class="img-responsive"></a>
+        </div>
+        <div class="openings_tijden">
+          <div class="openings_tijden_r1">Openingstijden:<br></div><div class="openings_tijden_r2"> Zondag &amp; Maandag<br></div><div class="openings_tijden_r3"> 10.00 &dash; 01.00 uur<br></div>
+          <div class="openings_tijden_r4">Dinsdag t/m Donderdag<br></div><div class="openings_tijden_r5"> 10.00 &dash; 02.00 uur<br></div>
+          <div class="openings_tijden_r6"> Vrijdag &amp; Zaterdag<br></div><div class="openings_tijden_r7"> 10.00 &dash; 03.00 uur</div>
+        </div>
+        <div class="adres">
+          <div class="adres_r1"><p>Markt 11<br></div><div class="adres_r2">5611 EB Eindhoven<br></div><div class="adres_r3">T 040 243 3995<br></div><div class="adres_r4">info&commat;de&dash;vooruitgang.nl</div>
+    </div>
+
+    <div class="footer">
+    <div class="footer_text">&copy; 2016 DE VOORUITGANG
+      | EN IN BEHEER VAN KOEN SENGERS.</div>
+    </div>
+
     </body>
 
 </html>
