8aaffc3c10e439020765031d04a5353a47ed1bd1
Koen Sengers <k.sengers@gynzy.com>
2018-09-28 17:02:34 +0200

    test

--- src/menu.php
+++ src/menu.php
@@ -27,7 +27,7 @@
             <img src="images/RBDA_logo_shortlist_.jpg" alt="img"></a></div>
         </div>
 
-        <div class="content_container">
+        <div class="menu_kaart">
           <div class="hr"><hr></div>
           <div class="hr1"><hr></div>
           <div class="hr2"><hr></div>
